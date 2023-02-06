ad1a6ac6f866da3a6c8a3e6a32799fc2d29a9d54
JianJiaXian <122859083+JianJiaXian@users.noreply.github.com>
2023-02-07 00:44:34 +0800

    Add files via upload

---
+++ Variables_Statements_and Object_Types.py
@@ -0,0 +1,9 @@
+#HW2
+'''
+請實作一個變數攝氏（celsius_temperature）溫度並攝定初始值 22，
+另外一個華氏變數（fahrenheit_temperature）則是由攝氏溫度變數組成華氏= (攝氏)(9/5) + 32，
+請撰寫一個程式包含兩個變數，將攝氏轉成華氏溫度，並印出結果。
+'''
+celsius_temperature=22
+fahrenheit_temperature=celsius_temperature*(9/5)+32
+print(fahrenheit_temperature)
